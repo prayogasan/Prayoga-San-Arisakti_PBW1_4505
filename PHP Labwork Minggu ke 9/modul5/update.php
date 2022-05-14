7db96dff98775f5ef5e58bbd12b56015434f889e
prayogasan <91250131+prayogasan@users.noreply.github.com>
2022-05-14 16:38:27 +0700

    Add files via upload

---
+++ PHP Labwork Minggu ke 9/modul5/update.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Update Mahasiswa-2062</title>
+	<link rel="stylesheet" type="text/css" href="sidebar.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<?php include("config.php");?>
+</head>
+<body>
+	<div id="container">
+		<ul class="nav nav-pills nav-stacked sidebar">
+			 <li role="presentation"><a href="index.php">Tambah Mahasiswa</a></li>
+			 <li role="presentation"><a href="hapusmhs.php">Hapus Mahasiswa</a></li>
+			 <li role="presentation"><a href="lihatdata.php">Lihat Data</a></li>
+			 <li role="presentation"><a href="cari.php">Cari</a></li>
+			 <li role="presentation" class="active"><a href="update.php">Update</a></li>
+		</ul>
+		<div id="content">
+			<form class="form-horizontal" method="post">
+				<div class="form-group">
+				    <label for="inputEmail3" class="col-sm-2 control-label">NIM</label>
+				    <div class="col-sm-10">
+				      <input type="text" class="form-control" id="inputEmail3" placeholder="NIM" name="nim">
+				    </div>
+				</div>
+				<div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" class="btn btn-success" name="submit">Cari Mahasiswa</button>
+					</div>
+  				</div>
+			</form>
+
+			
+			<?php
+			if (isset($_POST['submit'])) :
+				$nim = $_POST['nim'];
+				$query = "SELECT * FROM mahasiswa WHERE NIM='$nim'";
+				$hasil = mysqli_query($conn, $query);
+				if ($hasil == true) :
+					foreach ($hasil as $v) :
+						$id_mhs = $v['id_mhs'];
+						$nim = $v['NIM'];
+						$nama = $v['Nama_mhs']; ?>
+						<form class="form-horizontal" method="post">
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">NIM</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" value="<?= $nim; ?>" name="nim">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Nama</label>
+								<div class="col-sm-10">
+									<input type="text" class="form-control" value="<?= $nama; ?>" name="nama">
+									<input type="hidden" name="id_mhs" class="form-control" value="<?= $id_mhs; ?>">
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="inputEmail3" class="col-sm-2 control-label">Nama</label>
+								<div class="col-sm-10">
+									<button type="submit" class="btn btn-success" name="update">Update</button>
+								</div>
+							</div>
+						</form>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php
+	if (isset($_POST['update'])) :
+		$id_mhs = $_POST['id_mhs'];
+		$nim_update = $_POST['nim'];
+		$nama_update = $_POST['nama'];
+		$query = "UPDATE mahasiswa SET NIM='$nim_update', Nama_mhs='$nama_update' WHERE id_mhs='$id_mhs'";
+		$update = $conn->query($query);
+		if ($update == true) : ?>
+			<script type="text/javascript">
+				alert('Data berhasil diupdate!');
+			</script>
+		<?php endif; ?>
+	<?php endif; ?>
+</body>
+
+</html>
